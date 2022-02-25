cea328e325e2b54f948ae502b76b84c2adf2fbb2
Савчук Тарас <savxuk-2002@mail.ru>
2022-02-25 12:34:58 +0300

    добавлено задание 2, оба задания в виде слайдеров

---
+++ javascript/sliders.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $('.slider-task-1').slick({
+    infinite: true,
+    dots: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+  });
+});
+$(document).ready(function(){
+  $('.slider-task-2').slick({
+    infinite: true,
+    dots: true,
+    slidesToScroll: 1,
+    slidesToShow: 1,
+  });
+});
